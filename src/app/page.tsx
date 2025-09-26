41a9c952739b1e074fc40d341c9ba558027f3098
Fernando Josue Tocas Nores <fernando141255@hotmail.com>
2025-09-26 10:35:55 +0200

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,14 +14,14 @@ export default function Home() {
         />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
+            QUIERO SER EL QUINTO{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
+              TELETUBIE
             </code>
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            NUEVO COMENTARIO.
           </li>
         </ol>
 
